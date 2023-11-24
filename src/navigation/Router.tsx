f1a41913e308583b2d2c8ef9543d8f430ec7c8bb
jacky-wong9273 <wong97jacky77@gmail.com>
2023-11-24 16:07:56 +0800

    Revert "feat: added onboarding tabs"
    This reverts commit c3f1795ebec60e1496729885b3720bf7f3139172.

--- src/navigation/Router.tsx
+++ src/navigation/Router.tsx
@@ -4,23 +4,17 @@ import React from "react";
 import HomeTabNavigator from "./TabNavigator/HomeTabNavigator";
 
 // import screens
-import OnBoardingScreen from "../screens/OnBoardingScreen";
+import HomeScreen from "../screens/HomeScreen";
 
 // import main stack creator
 import { createStackNavigator } from "@react-navigation/stack";
+
 
 const MainStack = () => {
   const Stack = createStackNavigator();
 
   return (
     <Stack.Navigator>
-      <Stack.Screen
-        name={"On Board"}
-        component={OnBoardingScreen}
-        options={{
-          headerShown: false,
-        }}
-      />
       <Stack.Screen
         name={"HomeTab"}
         component={HomeTabNavigator}
